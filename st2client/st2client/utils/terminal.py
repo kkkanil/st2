20e21f3035a563f7f68a43d9fc28f946731dfa35
kkkanil <69108472+kkkanil@users.noreply.github.com>
2020-11-24 12:15:55 +0530

    Update terminal.py
            call = fcntl.ioctl(0, termios.TIOCGWINSZ, "\x00"*8)
            height, width = struct.unpack("hhhh", call)[:2]
            return width

--- st2client/st2client/utils/terminal.py
+++ st2client/st2client/utils/terminal.py
@@ -67,9 +67,9 @@ def get_terminal_size_columns(default=DEFAULT_TERMINAL_SIZE_COLUMNS):
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         print("ANIL in ioctl_GWINSZ"+str(st))
         sys.stdout.flush()
-        packed = fcntl.ioctl(fd, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0))
-        rows, cols, _, _ = struct.unpack('HHHH', packed)
-        return cols
+        call = fcntl.ioctl(0, termios.TIOCGWINSZ, "\x00"*8)
+        height, width = struct.unpack("hhhh", call)[:2]
+        return width
         # return struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, b"\x00" * 4))
     
     # 2. try stdin, stdout, stderr
